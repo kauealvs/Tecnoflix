d5ea434dad3216d35152985ebec6972561f00c15
Kauealvs <kauealvesdesena@gmail.com>
2020-08-05 13:03:47 -0300

    bug fix URL heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://db-tecnoflix.herokuapp.com/categorias';
+  : 'https://db-tecnoflix.herokuapp.com';
 
 export default {
   URL_BACKEND,
